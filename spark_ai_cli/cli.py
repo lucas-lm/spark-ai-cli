86990601448f908539bf90c4375a8bd5c8a1f1b3
Lucas Miranda <lucas-m@outlook.com>
2023-07-08 18:41:09 -0300

    feat: remove default limit of 5
    Now there is no limit of data to be displayed. We can still pass `--limit` flag yet to show only a limited number of rows

--- spark_ai_cli/cli.py
+++ spark_ai_cli/cli.py
@@ -6,7 +6,7 @@ DEFAULT_GPT_MODEL_NAME = 'gpt-3.5-turbo'
 app = typer.Typer()
 
 @app.command()
-def explore(extract: str, transform: str = None, limit: int = 15, gpt_model_name=DEFAULT_GPT_MODEL_NAME):
+def explore(extract: str, transform: str = None, limit: int = None, gpt_model_name=DEFAULT_GPT_MODEL_NAME):
   spark_ai = create_spark_ai_with_gpt(gpt_model_name)
   df = spark_ai.create_df(extract)
   print(df.ai.explain())
@@ -15,7 +15,7 @@ def explore(extract: str, transform: str = None, limit: int = 15, gpt_model_name
     df = df.ai.transform(transform)
     print(df.ai.explain())
   
-  df.show(n=limit)
+  df.show() if limit is None else df.show(n=limit)
 
 def main():
   app()
